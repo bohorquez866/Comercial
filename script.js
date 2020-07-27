c4f15f9bb0c0bee3fdfd500422afe02026c125b3
Jesus Bohorquez <bohorquez866@gmail.com>
2020-07-26 22:21:48 -0500

    page 2 real info missing

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const burguerMenu = document.querySelector('.burger-menu');
 const navbarList = document.querySelector('.navbar__list');
-const accordionItemHeaders = document.querySelectorAll('.accordion-header');
+const accItemHead = document.querySelectorAll('.accordion-header');
 
-accordionItemHeaders.forEach(accordionItemHeader => {
-    accordionItemHeader.addEventListener("click", event => {
-        const aciveAC = document.querySelector(".accordion-header.active");
-        if (aciveAC && aciveAC !== accordionItemHeader) {
-            aciveAC.classList.toggle("active");
-            aciveAC.nextElementSibling.style.maxHeight = 0;
+
+accItemHead.forEach(accItem => {
+    accItem.addEventListener("click", event => {
+        const activeAC = document.querySelector(".accordion-header.active");
+        if (activeAC && activeAC !== accItem) {
+            activeAC.classList.toggle("active");
+            activeAC.nextElementSibling.style.maxHeight = 0;
         }
-        accordionItemHeader.classList.toggle("active");
-        const accordionItemBody = accordionItemHeader.nextElementSibling;
-        if (accordionItemHeader.classList.contains("active")) {
+
+        accItem.classList.toggle("active");
+
+        const accordionItemBody = accItem.nextElementSibling;
+
+        if (accItem.classList.contains("active")) {
             accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-        } else {
+        } else if (!accItem.classList.contains("active")) {
             accordionItemBody.style.maxHeight = 0;
         }
     });
